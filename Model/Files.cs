163b852837d483e0b87bef8420a153d912961d3a
SergeyZhiganov <isip_s.a.zhiganov@mpt.ru>
2024-05-03 12:25:43 +0300

    Практа

---
+++ Model/Files.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Будни_Программиста.Model
+{
+    internal static class Files
+    {
+        public static void Serialize<T>(T type, string path)
+        {
+            var json = JsonConvert.SerializeObject(type);
+            File.WriteAllText(path, json);
+        }
+        public static T? Deserialize<T>(string path)
+        {
+            var json = File.ReadAllText(path);
+            var type = JsonConvert.DeserializeObject<T>(json);
+            return type;
+        }
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Будни_Программиста.ViewModel;
+
+namespace Будни_Программиста.View
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var viewModel = new MainWindowViewModel();
+            DataContext = viewModel;
+            MainWindowViewModel.GetData();
+            MainWindowViewModel.framePage.Content = new DaysPage()
+            {
+                DataContext = viewModel
+            };
+        }
+    }
+}
